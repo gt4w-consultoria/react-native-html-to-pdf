bc331d3c8a49e5ffa3fe0833f41e9c5c54993185
Jean Santos Diniz <jeansantosdiniz2001@gmail.com>
2023-05-18 10:11:28 -0300

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,27 +1,29 @@
-export interface Options {
-    html: string;
-    fileName?: string;
-    base64?: boolean;
-    directory?: string;
-    height?: number;
-    width?: number;
-  
-    // iOS only
-    paddingLeft?: number;
-    paddingRight?: number;
-    paddingTop?: number;
-    paddingBottom?: number;
-    padding?: number;
-    bgColor?: string;
-  
-    // android only
-    fonts?: string[];
+declare module 'react-native-html-to-pdf' {
+    export interface Options {
+        html: string;
+        fileName?: string;
+        base64?: boolean;
+        directory?: string;
+        height?: number;
+        width?: number;
+
+        // iOS only
+        paddingLeft?: number;
+        paddingRight?: number;
+        paddingTop?: number;
+        paddingBottom?: number;
+        padding?: number;
+        bgColor?: string;
+
+        // android only
+        fonts?: string[];
+      }
+
+      export interface Pdf {
+        filePath?: string;
+        numberOfPages?: number;
+        base64?: string;
+      }
+
+      export function convert(options: Options): Promise<Pdf>
   }
-  
-  export interface Pdf {
-    filePath?: string;
-    numberOfPages?: number;
-    base64?: string;
-  }
-  
-  export function convert(options: Options): Promise<Pdf>
